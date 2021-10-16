e6d76a5ae747ba8a8d7a3b0177d0c8db2d562f9e
ramakrishnavelisetti <ramakrishna96.velisetti@gmail.com>
2021-10-16 17:19:44 +0530

    Added: append data at end of node

--- src/main/java/com/bdlz/linkedlist/LinkedListMain.java
+++ src/main/java/com/bdlz/linkedlist/LinkedListMain.java
@@ -5,11 +5,16 @@ import java.util.Scanner;
 public class LinkedListMain {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter 1 to add the data at the start");
+        System.out.println("Enter 1 to add the data at the start\n" +
+                            "Enter 2 to add the data at the end");
         switch(scanner.nextInt()) {
             case 1:
                 Operation.addDataAtStart();
-                scanner.close();
+                break;
+            case 2:
+                Operation.addDataAtEnd();
+                break;
         }
+        scanner.close();
     }
 }
