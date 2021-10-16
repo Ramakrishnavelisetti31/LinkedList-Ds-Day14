f36fcff6973f07cdbf1763476f934b5f0140358a
ramakrishnavelisetti <ramakrishna96.velisetti@gmail.com>
2021-10-16 13:38:18 +0530

    Added: Welcome message

---
+++ src/main/java/com/bdlz/linkedlist/LinkedListMain.java
@@ -0,0 +1,8 @@
+package com.bdlz.linkedlist;
+
+public class LinkedListMain {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Data Structures Linked list program");
+    }
+}
