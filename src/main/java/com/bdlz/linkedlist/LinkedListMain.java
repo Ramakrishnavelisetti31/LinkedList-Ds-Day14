fac5ce926e3764522dfdd48d5aa4e53fc1bf7021
ramakrishnavelisetti <ramakrishna96.velisetti@gmail.com>
2021-10-16 15:39:20 +0530

    Added: created a linked list

--- src/main/java/com/bdlz/linkedlist/LinkedListMain.java
+++ src/main/java/com/bdlz/linkedlist/LinkedListMain.java
@@ -1,8 +1,22 @@
 package com.bdlz.linkedlist;
 
 public class LinkedListMain {
-
     public static void main(String[] args) {
-        System.out.println("Welcome to the Data Structures Linked list program");
+
+        Node firstNode = new Node(56);
+        Node secondNode = new Node(70);
+        Node thirdNode = new Node(30);
+
+
+        Node head = firstNode;
+        firstNode.next = secondNode;
+        secondNode.next = thirdNode;
+        Node tail = thirdNode;
+
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.data);
+            temp = temp.next;
+        }
     }
 }
