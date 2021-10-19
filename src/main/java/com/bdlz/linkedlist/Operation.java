1ffd854a1b3227534d3ec47428b37efeaff7b890
ramakrishnavelisetti <ramakrishna96.velisetti@gmail.com>
2021-10-20 00:00:18 +0530

    Added: Find node based on key value

--- src/main/java/com/bdlz/linkedlist/Operation.java
+++ src/main/java/com/bdlz/linkedlist/Operation.java
@@ -40,4 +40,8 @@ public class Operation {
         linkedList.print();
 
     }
+    public void findNodeBasedOnValue() {
+        addDataAtStart();
+        linkedList.findNode(30);
+    }
 }
---
+++ src/test/java/com/bdlz/linkedlist/LinkedListMainTest.java
@@ -0,0 +1,12 @@
+package com.bdlz.linkedlist;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LinkedListMainTest {
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue( true );
+    }
+}
